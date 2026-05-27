217032bc2dc60fd847503880dc4e5159e86264cc
FiscalHN Developer <soportetecnico@mpl.hn>
2026-05-26 20:55:46 -0600

    feat: add shared state module

---
+++ js/state.js
@@ -0,0 +1,9 @@
+export const state = {
+    currentOwnerId: null,
+    currentUserUid: null,
+    allDocuments: [],
+    myDocuments: [],
+    thankedReportIds: new Set(),
+    notifiedMatches: new Set(),
+    viewingFinderInfo: new Set(),
+};
